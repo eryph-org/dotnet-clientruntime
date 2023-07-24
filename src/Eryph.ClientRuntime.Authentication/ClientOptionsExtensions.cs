c0196023c21c2679d3780b9d1c7c87d26edfc6f0
Frank Wagner <frank2568@gmail.com>
2023-07-24 10:20:11 +0200

    upgrade to autorest v3 (#31)
    * upgrade to autorest v3
    * dependency updates

---
+++ src/Eryph.ClientRuntime.Authentication/ClientOptionsExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Core;
+using Azure.Core.Pipeline;
+using Eryph.ClientRuntime.Configuration;
+using Eryph.IdentityModel.Clients;
+
+namespace Eryph.ClientRuntime.Authentication
+{
+    public static class ClientOptionsExtensions
+    {
+
+        public static HttpPipeline BuildHttpPipeline(this ClientOptions options, ClientCredentials credentials, string scope)
+        {
+            var tokenCredential = new EryphTokenCredential(credentials);
+
+            var authPolicy = new BearerTokenAuthenticationPolicy(tokenCredential, scope);
+            return HttpPipelineBuilder.Build(options, authPolicy);
+        }
+
+        public static HttpPipeline BuildHttpPipeline(this ClientOptions options, IEnvironment sysEnv, string scope)
+        {
+            var tokenCredential = new EryphTokenCredential(
+                new ClientCredentialsLookup(sysEnv).GetDefaultCredentials());
+
+            var authPolicy = new BearerTokenAuthenticationPolicy(tokenCredential, scope);
+            return HttpPipelineBuilder.Build(options, authPolicy);
+        }
+    }
+}
---
+++ src/Eryph.ClientRuntime.Authentication/EryphTokenCredential.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Eryph.IdentityModel.Clients;
+
+namespace Eryph.ClientRuntime.Authentication
+{
+    internal class EryphTokenCredential : TokenCredential
+    {
+        private readonly ClientCredentials _clientCredentials;
+        private readonly HttpClient _httpClient;
+
+        public EryphTokenCredential(ClientCredentials clientCredentials, HttpClient httpClient = null)
+        {
+            _clientCredentials = clientCredentials;
+            _httpClient = httpClient;
+        }
+
+        public override async ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            var response = await _clientCredentials.GetAccessToken(requestContext.Scopes.ToArray(), _httpClient);
+            return new AccessToken(response.AccessToken, response.ExpiresOn.GetValueOrDefault());
+        }
+
+        public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            return GetTokenAsync(requestContext, cancellationToken).GetAwaiter().GetResult();
+        }
+
+    }
+}
