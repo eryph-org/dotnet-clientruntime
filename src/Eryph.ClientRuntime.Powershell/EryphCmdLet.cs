cab0353a42c368ac22abec56975f1d728c368bf1
Frank Wagner <frank2568@gmail.com>
2026-05-31 11:08:00 +0200

    add support for session wide credentials (#65)

--- src/Eryph.ClientRuntime.Powershell/EryphCmdLet.cs
+++ src/Eryph.ClientRuntime.Powershell/EryphCmdLet.cs
@@ -24,6 +24,8 @@ namespace Eryph.ClientRuntime.Powershell
             var clientCredentials = Credentials;
             if (clientCredentials != null) return clientCredentials;
 
+            if (SessionState.PSVariable.GetValue("EryphSessionCredentials") is ClientCredentials sessionCredentials)
+                return sessionCredentials;
 
             var lookup = new ClientCredentialsLookup(new PowershellEnvironment(SessionState));
             clientCredentials = lookup.FindCredentials();
@@ -31,7 +33,7 @@ namespace Eryph.ClientRuntime.Powershell
             if (clientCredentials == null)
             {
                 throw new InvalidOperationException(@"Could not find credentials for eryph.
-You can use the parameter Credentials to set the eryph credentials. If not set, the credentials will be searched in your local configuration. 
+You can use the parameter Credentials to set the eryph credentials. If not set, the credentials will be searched in your local configuration.
 If there is no default eryph client in your configuration the command will try to access the default system-client of a local running eryph zero or identity server.
 To access the system-client you will have to run this command as Administrator (Windows) or root (Linux).
 ");
